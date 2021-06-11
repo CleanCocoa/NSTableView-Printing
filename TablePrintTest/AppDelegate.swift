42a7ca5ec00b3af6aa5f646f650b70280d6cc35f
Christian Tietze <me@christiantietze.de>
2021-06-11 19:50:32 +0200

    Initial Commit

---
+++ TablePrintTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TablePrintTest
+//
+//  Created by Christian Tietze on 11.06.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
