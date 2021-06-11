489d9ef1ef5dc7fbfaa1260e410f82f69785362b
Christian Tietze <me@christiantietze.de>
2021-06-11 20:33:43 +0200

    populate table with dummy data

--- TablePrintTest/ViewController.swift
+++ TablePrintTest/ViewController.swift
@@ -7,20 +7,28 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    /*
+     Wir simulieren hier 1000 Zeilen in der Tabelle, um den Druck über
+     mehrere Seiten zu betrachten. Statt wirklich 1000 Objekte anzulegen,
+     werden die on-the-fly erstellt.
+     */
 
-        // Do any additional setup after loading the view.
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 1000
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return row
     }
 
 
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let cellView = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier("cell"), owner: nil) as? NSTableCellView else { return nil }
+        let value = self.tableView(tableView, objectValueFor: tableColumn, row: row) ?? 0
+        cellView.textField?.stringValue = "\(value)"
+        return cellView
+    }
 }
 
