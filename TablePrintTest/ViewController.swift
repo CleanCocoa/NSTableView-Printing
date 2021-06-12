0553f593e37f087c3352e00650be260f700f05f1
Christian Tietze <me@christiantietze.de>
2021-06-12 09:02:45 +0200

    customize printing table to a page

--- TablePrintTest/ViewController.swift
+++ TablePrintTest/ViewController.swift
@@ -9,6 +9,45 @@ import Cocoa
 
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
+    @IBOutlet var tableView: NSTableView!
+
+    @IBAction func printTableWithDecoration(_ sender: Any?) {
+        // MARK: Druckbereich ermitteln, um die Größe der NSViews auf Papier zu bestimmen
+        // Verfügbare Maße zum Drucken zunächst auf den druckbaren Bereich
+        // einer Seite beschränken -- wir erlauben später, vertikal zu wachsen.
+        let printInfo = NSPrintInfo.shared
+        let paperSize = NSPrintInfo.shared.paperSize // Maße einer ganzen Seite (A4) bis zum Rand, also mehr als man bedrucken kann
+        let pageContentSize = NSSize(width: paperSize.width - printInfo.leftMargin - printInfo.rightMargin,
+                                     height: paperSize.height - printInfo.topMargin - printInfo.bottomMargin)
+
+        // MARK: Anordnen der Elemente auf der Seite
+        // Einleitungstext und Tabelle werden vertikal auf der Seite angeordnet, in einem 'stack' der die Seite füllt
+        let initialFrameForPrinting = NSRect(origin: .zero, size: pageContentSize)
+        let stackView = NSStackView(frame: initialFrameForPrinting)
+        stackView.orientation = .vertical
+        stackView.alignment = .left
+        stackView.autoresizingMask = [.height] // Container darf größer werden, wenn z.B. die Tabelle zu lang wird
+
+        // Die Komponenten in den Stack einfügen:
+        stackView.addArrangedSubview(self.tableView)
+
+        // Forcieren des Layouts
+        stackView.layoutSubtreeIfNeeded()
+
+        // MARK: Drucken der View
+        // Drucken oben-links auf der Seite beginnen, egal wie klein der Inhalt ist
+        printInfo.isHorizontallyCentered = false
+        printInfo.isVerticallyCentered = false
+
+        printInfo.horizontalPagination = .clip  // .fit schrumpft alles, bis es passt
+        printInfo.verticalPagination = .automatic
+
+        let printOperation = NSPrintOperation(view: stackView)
+        printOperation.runModal(for: self.view.window!, delegate: nil, didRun: nil, contextInfo: nil)
+
+        // Am Ende dieser Funktion werden alle lokalen Variablen freigegeben und die `stackView` und `printOperation` automatisch gelöscht.
+    }
+
     /*
      Wir simulieren hier 1000 Zeilen in der Tabelle, um den Druck über
      mehrere Seiten zu betrachten. Statt wirklich 1000 Objekte anzulegen,
